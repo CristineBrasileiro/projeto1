be41cec114f0b0846198405a2442d80aa2d41ee3
Anelle C. B. Valença <anellevalenca@gmail.com>
2025-03-21 21:09:44 -0300

    changes

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+# from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'home.html')
+    return HttpResponse('HOME')
 
 
 def sobre(request):
